6815539f3487f1878b08aa5784efa618777d36c1
Real Phoenix <phoenix@therealphoenix.ga>
2021-01-29 23:15:31 +0530

    Add example

---
+++ example/example.dart
@@ -0,0 +1,19 @@
+import 'package:myanimelist_api/myanimelist_api.dart';
+
+void main() async {
+  // Intiialize client
+  var client = Client("YOUR_ACCESS_TOKEN");
+
+  // Search for anime
+  var animeList = await client.searchAnime("Shingeki no Kyojin");
+  for (var anime in animeList) {
+    print(anime.id); // Most methods return objects with minimal fields
+  }
+
+  // Get complete objects using id
+  var anime = await client.getAnimeDetails(16498);
+  assert(anime.title == "Shingeki no Kyojin");
+  assert(anime.mainPicture.large == "https://api-cdn.myanimelist.net/images/anime/10/47347l.jpg");
+  assert(anime.genres[0].name == "Action");
+  assert(anime.createdAt == DateTime.parse("2012-12-05T12:03:21.000Z"));
+}
